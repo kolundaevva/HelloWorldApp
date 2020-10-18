19fc91524180fc167ac6691d9dc80079e720def7
Vladislav Kolundaev <kolundaevva@gmail.com>
2020-10-18 13:48:01 +0300

    Work with logic in view controller

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redColorTrafficLightView: UIView!
+    @IBOutlet var yellowColorTrafficLightView: UIView!
+    @IBOutlet var greenColorTrafficLightView: UIView!
+    
+    @IBOutlet var switcherButtor: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redColorTrafficLightView.alpha = 0.3
+        yellowColorTrafficLightView.alpha = 0.3
+        greenColorTrafficLightView.alpha = 0.3
+        
+        redColorTrafficLightView.layer.cornerRadius = 55
+        yellowColorTrafficLightView.layer.cornerRadius = 55
+        greenColorTrafficLightView.layer.cornerRadius = 55
+        switcherButtor.layer.cornerRadius = 10
     }
 
-
+    @IBAction func switchButton() {
+        if redColorTrafficLightView.alpha != 1, yellowColorTrafficLightView.alpha != 1{
+            greenColorTrafficLightView.alpha = 0.3
+            redColorTrafficLightView.alpha = 1
+            switcherButtor.setTitle("Next", for: .normal)
+        } else if yellowColorTrafficLightView.alpha != 1 {
+            redColorTrafficLightView.alpha = 0.3
+            yellowColorTrafficLightView.alpha = 1
+            switcherButtor.setTitle("Next", for: .normal)
+        } else {
+            redColorTrafficLightView.alpha = 0.3
+            yellowColorTrafficLightView.alpha = 0.3
+            greenColorTrafficLightView.alpha = 1
+            switcherButtor.setTitle("Next", for: .normal)
+        }
+    }
+    
 }
 
